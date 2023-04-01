90cbeec66e46f0143d5776d50caba6feb475c5c5
Berend Folkers <16764030+berend109@users.noreply.github.com>
2023-04-01 18:31:49 +0200

    hasj function

---
+++ WebdevProjectStarterTemplate/Helpers/Hasj.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WebdevProjectStarterTemplate.Helpers
+{
+	public class Hasj
+	{
+		public static string HasjPassword(string password)
+		{
+			using var sha256 = SHA256.Create();
+			var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+			return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+		}
+	}
+}
