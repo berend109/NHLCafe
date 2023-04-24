50e601b6971802703dd09618ece7696c8e34e90b
berend109 <berend109@gmail.com>
2023-04-24 16:39:23 +0200

    drankenkaart eerste design

--- WebdevProjectStarterTemplate/Pages/Drankenkaart.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Drankenkaart.cshtml.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebdevProjectStarterTemplate.Models;
+using WebdevProjectStarterTemplate.Repositories;
 
 namespace WebdevProjectStarterTemplate.Pages;
 
 public class Drankenkaart : PageModel
 {
+    public IEnumerable<Product> ProductWithCategory { get; set; } = null!;
+    public IEnumerable<Category> Categories { get; set; } = null!;
     public void OnGet()
     {
-        
+        ProductWithCategory = new ProductRepository().GetProductWithCategory(); 
+        Categories = new CategoryRepository().GetCategoriesWithProducts();
     }
 }
