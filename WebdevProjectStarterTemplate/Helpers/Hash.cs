16b4c793510093c855fb3e4f2d59350b180eb897
berend109 <berend109@gmail.com>
2023-07-05 04:03:22 +0200

    small tweak

--- WebdevProjectStarterTemplate/Helpers/Hash.cs
+++ WebdevProjectStarterTemplate/Helpers/Hash.cs
@@ -8,6 +8,7 @@ namespace WebdevProjectStarterTemplate.Helpers
 		public static string HashPassword(string? password)
 		{
 			using var sha256 = SHA256.Create();
+			if (password == null) return "";
 			var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
 			return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
 		}
